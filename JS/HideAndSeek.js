5287ea6fb2265edd04b83359733bd0e011eacd22
Tibor Áser Veres (RPBCACUEAIIBH) <admin@osrc.rip>
2024-02-08 18:18:57 +0200

    Removed some extra spaces, hook didn't work, trying post-receive...

--- JS/HideAndSeek.js
+++ JS/HideAndSeek.js
@@ -11,13 +11,13 @@ function ShowElementInline (ElementID)
 function HideClass (ClassName)
 {
     Elements = document.getElementsByClassName (ClassName);
-    for (var i = 0; i < Elements.length; i++) 
+    for (var i = 0; i < Elements.length; i++)
         Elements[i].style.display = "none";
 }
 
 function ShowClassInline (ClassName)
 {
     Elements = document.getElementsByClassName (ClassName);
-    for (var i = 0; i < Elements.length; i++) 
+    for (var i = 0; i < Elements.length; i++)
         Elements[i].style.display = "inline";
 }
